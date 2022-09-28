88fb7e3114fce8dbe8ad1f4a099080095c102e61
AndrewI997 <andre.a1997@mail.ru>
2022-09-28 15:47:17 +0500

    axios_sending_form_in_telegram_channel

--- src/components/shared/modals/modalOrder/ModalOrder.tsx
+++ src/components/shared/modals/modalOrder/ModalOrder.tsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import axios from 'axios'
+import React, { MouseEventHandler } from 'react'
 import ModalWrap from '../modalWrap/ModalWrap'
 import s from './modalOrder.module.scss'
 
 const ModalOrder = ({ active, setActive }: any) => {
+
+    const [phoneNumber, setPhoneNumber] = React.useState('')
+    const [personName, setPersonName] = React.useState('')
+
+    const sendToTelegram = (e: any) => {
+        e.preventDefault()
+
+        const TOKEN = '5637045049:AAFuEEJIwdRqqPeUmsVtwPAN1p0-0sqh9bw'
+        const CHAT_ID = '-1001814307663'
+        const TG_API_URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`
+
+        let message = `<b>Ура! Новая заявка!</b>\n`
+        message += `<b>Имя отправителя:</b> ${personName}\n`
+        message += `<b>Номер отправителя:</b> ${phoneNumber}`
+
+        axios.post(TG_API_URL, {
+            chat_id: CHAT_ID,
+            parse_mode: 'html',
+            text: message
+        })
+        setPersonName('')
+        setPhoneNumber('')
+        setActive(false)
+
+    }
     
     return (
         <ModalWrap active={active} setActive={setActive}>
             <div className={s.formInner}>
                 <h3>Обратный звонок</h3>
-                <input type="text" placeholder="Имя" />
-                <input type="email" placeholder="Телефон" />
+                <input type="text" placeholder="Имя" value={personName} onChange={(e) => setPersonName(e.target.value)} />
+                <input type="email" placeholder="Телефон" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
                 <textarea placeholder="Сообщение..." rows={3}></textarea>
-                <input type="submit" value="Отправить" />
+                <input type="submit" value="Отправить" onClick={(e)=>sendToTelegram(e)} />
             </div>
         </ModalWrap>
     )
