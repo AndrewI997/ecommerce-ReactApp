8fa37f0b3fc23c1649e762da03a9cc879267f0fa
AndrewI997 <andre.a1997@mail.ru>
2022-10-25 19:14:14 +0500

    reduxtoolkit_config

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
-// ...
-const store = configureStore({
-  reducer: {
+import { combineReducers } from '@reduxjs/toolkit'
 
-  },
+
+const rootReducer = combineReducers({
+    
 })
-export type RootState = ReturnType<typeof store.getState>
+
+const store = configureStore({
+  reducer: rootReducer,
+})
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppDispatch = typeof store.dispatch
 
 export default store
